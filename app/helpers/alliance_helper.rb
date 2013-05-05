50a19161eb066a5da52abdd831cbb906961e0558
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-05-05 10:54:24 +0100

    Delete helper

--- app/helpers/alliance_helper.rb
+++
@@ -1,8 +0,0 @@
-module AllianceHelper
-  require 'json'
-  require 'httparty'
-
-  def getAlliance(id)
-    JSON.parse HTTParty.get('http://evewho.com/api.php?type=allilist&id=' + id.to_s).response.body
-  end
-end
