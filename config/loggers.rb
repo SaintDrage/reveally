56897371bfc71bb3ed7dbbe3ade0338484e13679
Ivan Kotov <i.s.kotov@gmail.com>
2013-05-06 16:09:14 +0400

    Created corporatiions models from EVE API

--- config/loggers.rb
+++
@@ -1,8 +0,0 @@
-class EveApiLogFormatter < Logger::Formatter
-  def format_message(severity, time, progname, msg)
-    "[%s(%d)%5s] %s\n" % [time.to_s(:short), $$, severity, msg2str(msg)]
-  end
-end
-
-eveapi_log = Logger.new(Rails.root.join('log', 'eveapi.log'), 'daily', 30*1024*1024)
-eveapi_log.formatter = EveApiLogFormatter.new
