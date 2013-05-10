f0af4c812f81b5fc99bc2c95b64b41864b5eb78e
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-05-10 08:41:16 +0100

    Add keys to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 ALLY_ID = '99002003'
 
-puts "✩✩✩✩ Create corporations ✩✩✩✩"
+puts "✩✩✩✩ Create Corporations ✩✩✩✩"
 include EveApiHelper
 list = alliance_list
 list.alliances.each do |alliance|
@@ -26,4 +26,18 @@ list.alliances.each do |alliance|
        puts " -> Corporation '#{info.corporationName}' has been created!"
      end
   end
+
+  puts "✩✩✩✩ Create Corporation Keys ✩✩✩✩"
+
+  key = {
+    id: 1234567,
+    v_code: 'goin34342oi4n3f23o4nrou3242dj4nr2i34br4bt3i2rpb34jrn324kjfn2432g'
+  }
+  key = CorporationKey.create(key)
+
+  # Add key to first corporation.
+  corporation = Corporation.first
+
+  corporation.corporation_keys = [key]
+  corporation.save!
 end
